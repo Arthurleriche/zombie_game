b251adc91f9141b4b97e9db09f799fd30b1edd77
Arthurleriche <lerichearthur@gmail.com>
2020-10-30 15:07:41 +0100

    done

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::UsersController < ApplicationController
+      skip_before_action :verify_authenticity_token
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: { error: user.errors.messages}, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :coin, :level)
+  end
+end
